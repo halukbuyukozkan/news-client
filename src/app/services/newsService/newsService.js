a09eb6639d66f828aaee893c702b98353170737a
halukbuyukozkan <halukbuyukozkan@gmail.com>
2023-07-24 20:57:21 +0300

    mui table example added

---
+++ src/app/services/newsService/newsService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const news_url = "api/news";
+
+export async function getUsers() {
+  return new Promise((resolve, reject) => {
+    axios.get(news_url).then((response) => {
+      if (response) {
+        resolve(response.data);
+      } else {
+        reject(response.data.error);
+      }
+    });
+  });
+}
